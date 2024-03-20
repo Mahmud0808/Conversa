3874b6bf30c308883ba49510abddbfb6d97834dd
DrDisagree <crazymahmud08@gmail.com>
2024-03-21 02:46:30 +0600

    Move extractNativeLibs to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,11 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    packaging {
+        jniLibs {
+            useLegacyPackaging = true
+        }
+    }
 }
 
 dependencies {
